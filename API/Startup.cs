de433f0f3d4174bf029ae68a479eabef7d46c436
Megan Hepner <megan.hepner@gmail.com>
2022-11-21 20:55:13 -0800

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,11 @@ namespace API
             {
                 OptionsBuilderConfigurationExtensions.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +52,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
