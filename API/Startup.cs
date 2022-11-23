65ff908988cc53fd7d462945883b1c4ac3b02a25
Megan Hepner <megan.hepner@gmail.com>
2022-11-22 19:33:31 -0800

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,11 +1,16 @@
+using API.Extensions;
+using Application.Activities;
+using Application.Core;
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Persistence;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Persistence;
-using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -24,19 +29,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(OptionsBuilderConfigurationExtensions =>
-            {
-                OptionsBuilderConfigurationExtensions.UseSqlite(_config.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy =>{
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
+            services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
